3e93c28e5caf29986531a24a107cb09c8e8eb8d0
Alessandro Carucci <alessandrocarucci.ac@gmail.com>
2025-03-24 16:45:32 +0000

    update orders API

--- orders/API/api_orders.py
+++ orders/API/api_orders.py
@@ -11,14 +11,15 @@ import schedule
 login_url = "https://app.mailship.eu/api/login/user"
 refresh_url = "https://app.mailship.eu/api/refresh-token"
 
-# API Call - Reservations
-reservation_list_url = "https://app.mailship.eu/api/reservation/list"  
+# API Call - Expedition and Carrier
+expedition_list_url = "https://app.mailship.eu/api/expedition/list"
+moviment_list_url = "https://app.mailship.eu/api/stock-movement/list"
 
 # Login Credentials
 email = "alessandrocarucci.ac@gmail.com"
-password = "Alex260981"  
+password = "Alex260981"
 
-# SFTP 
+# SFTP
 hostname = 'ws.italagro.bindcommerce.biz'
 port = 22
 sftp_username = 'wsitalagro'
@@ -37,14 +38,13 @@ def login():
 
     if response.status_code == 200:
         token_data = response.json()
-        token = token_data.get('token')         
+        token = token_data.get('token')
         expires_in = token_data.get('expires_in')
 
         if expires_in is None:
             expires_in = 3600  # durata del token (1 ora)
 
         return token, expires_in
-    else:
         print("Errore durante il login:", response.status_code, response.text)
         return None, None
 
@@ -52,7 +52,7 @@ token = login()
 print(f"Token ricevuto con successo: {token}")
 print("Login effettuato con successo!\n")
 
-# Refresh Token Function
+# Refresh Token
 def refresh_token(refresh_token):
     headers = {'Content-Type': 'application/json'}
     payload = {'refresh_token': refresh_token}
@@ -65,78 +65,59 @@ def refresh_token(refresh_token):
         print("Errore durante il refresh del token:", response.status_code, response.text)
         return None, None
 
-# Funzione per fare una richiesta POST a /reservation/list e ottenere i prodotti
-def get_reservation_list(token):
+# Funzione per fare una richiesta POST a /expedition/list e ottenere le spedizioni
+def get_expedition_list(token):
     headers = {
         'Authorization': f'Bearer {token}',  
         'Content-Type': 'application/json'
     }
     
-    response = requests.post(reservation_list_url, headers=headers)
+    response = requests.post(expedition_list_url, headers=headers)
 
     if response.status_code == 200:
         # Ottieni la risposta JSON
-        reservation_data = response.json()
-        print("Scarico la lista prodotti in formato JSON")
+        expedition_data = response.json()
+        print("Scarico la lista spedizioni in formato JSON")
 
         # Salva la risposta JSON in un file
-        with open('reservation_list.json', 'w') as f:
-            json.dump(reservation_data, f, indent=4)
-        print("Contenuto salvato in 'reservation_list.json'\n")
+        with open('expedition_list.json', 'w') as f:
+            json.dump(expedition_data, f, indent=4)
+        print("Contenuto salvato in 'expedition_list.json'\n")
 
-        return reservation_data.get('results', [])
+        return expedition_data.get('results', [])
     else:
-        print("Errore durante la richiesta a /reservation/list:", response.status_code, response.text)
+        print("Errore durante la richiesta a /expedition/list:", response.status_code, response.text)
         return []
 
-# Funzione per generare il CSV e caricarlo su SFTP
-def generate_csv_and_upload_to_sftp(product_list, product_stock):
-    timestamp = datetime.now().strftime('%d-%m-%Y_%H:%M:%S')  
-    remote_file_path = f'/home/wsitalagro/webapps/ws-italagro/orders/export_orders_api_{timestamp}.csv'  
+# Funzione per fare una richiesta POST a /moviment/list e ottenere la lista dei corrieri
+def get_moviment_list(token):
+    headers = {
+        'Authorization': f'Bearer {token}',  
+        'Content-Type': 'application/json'
+    }
     
-    print("Genero il CSV aggiornato...")
-    csv_buffer = StringIO()
-    fieldnames = ['Internal SKU', 'Primary EAN', 'Name', 'Quantity']
-    writer = csv.DictWriter(csv_buffer, fieldnames=fieldnames)
+    response = requests.post(moviment_list_url, headers=headers)
 
-    writer.writeheader()
+    if response.status_code == 200:
+        # Ottieni la risposta JSON
+        moviment_data = response.json()
+        print("Scarico la lista corrieri in formato JSON")
 
-        new_row = {
-            'Internal SKU': product.get('internalSku', ''),
-            'Primary EAN': product.get('productSku', ''),
-            'Name': product.get('name', ''),
-            'Quantity': product_quantity
-            }
-        writer.writerow(new_row)
+        # Salva la risposta JSON in un file
+        with open('moviment_list.json', 'w') as f:
+            json.dump(moviment_data, f, indent=4)
+        print("Contenuto salvato in 'moviment_list.json'\n")
 
-    csv_buffer.seek(0)
-
-    try:
-        # Connessione al server SFTP
-        transport = paramiko.Transport((hostname, port))
-        transport.connect(username=sftp_username, password=sftp_password)
-        
-        # Crea il client SFTP
-        sftp = paramiko.SFTPClient.from_transport(transport)
-        
-        # Scrive il CSV sul server SFTP
-        with sftp.open(remote_file_path, 'w') as remote_file:
-            remote_file.write(csv_buffer.getvalue())
-
-        print(f"CSV caricato con successo su SFTP: {remote_file_path}")
-        print("Attendo 5 minuti prima di un nuovo aggiornamento....\n")
-
-    except Exception as e:
-        print(f"Errore durante il caricamento del file CSV su SFTP: {e}")
-    finally:
-        # Chiudi la connessione SFTP
-        transport.close()
+        return moviment_data.get('results', [])
+    else:
+        print("Errore durante la richiesta a /moviment/list:", response.status_code, response.text)
+        return []
 
 # Funzione principale per gestire l'autenticazione e il refresh del token
 def authenticate():
     token, expires_in = login()
     
-    if token:  
+    if token:
         expiry_time = time.time() + expires_in
         
         while True:
@@ -150,16 +131,19 @@ def authenticate():
                 else:
                     print("Errore nel rinnovo del token.")
                     break
-            else:
 
-                reservation_list = get_reservation_list(token)    
-                generate_csv_and_upload_to_sftp(reservation_list)
-                break 
+                
+                expedition_list = get_expedition_list(token)
+                moviment_list = get_moviment_list(token)
+                break
 
 # Schedulazione salvataggio CSV ogni 5 minuti
-schedule.every(5).minutes.do(authenticate)
+schedule.every(1).minutes.do(authenticate)
 
 # Loop per eseguire la schedulazione
 while True:
     schedule.run_pending()
     time.sleep(10)  
+
+
+
