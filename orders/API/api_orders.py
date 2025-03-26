a0420cfb497e475748482709cb0a3421d55b30bb
Alessandro Carucci <alessandrocarucci.ac@gmail.com>
2025-03-26 19:06:14 +0000

    final review API Orders

--- orders/API/api_orders.py
+++ orders/API/api_orders.py
@@ -11,11 +11,11 @@ import schedule
 login_url = "https://app.mailship.eu/api/login/user"
 refresh_url = "https://app.mailship.eu/api/refresh-token"
 
-# API Call - Expedition & Carrier | Moviment & Product
+# API Call - Expedition & Carrier | WareHouse & Eshop
 expedition_list_url = "https://app.mailship.eu/api/expedition/list"
 carrier_list_url = "https://app.mailship.eu/api/carrier/list"
-moviment_list_url = "https://app.mailship.eu/api/stock-movement/list"
-product_list_url = "https://app.mailship.eu/api/product/list"  
+warehouse_list_url = "https://app.mailship.eu/api/warehouse/list"
+eshop_list_url = "https://app.mailship.eu/api/eshop/list"  
 
 # Login Credentials
 email = "alessandrocarucci.ac@gmail.com"
@@ -115,61 +115,60 @@ def get_carrier_list(token):
         print("Errore durante la richiesta a /carrier/list:", response.status_code, response.text)
         return []
 
-# Funzione per fare una richiesta POST a /moviment/list e ottenere la lista spedizioni assegnate ai corrieri
-def get_moviment_list(token):
+# Funzione per fare una richiesta POST a /warehouse/list e ottenere info magazzino
+def get_warehouse_list(token):
     headers = {
         'Authorization': f'Bearer {token}',  
         'Content-Type': 'application/json'
     }
     
-    response = requests.post(moviment_list_url, headers=headers)
+    response = requests.post(warehouse_list_url, headers=headers)
 
     if response.status_code == 200:
         # Ottieni la risposta JSON
-        moviment_data = response.json()
+        warehouse_data = response.json()
         print("Scarico la lista corrieri in formato JSON")
 
         # Salva la risposta JSON in un file
-        with open('moviment_list.json', 'w') as f:
-            json.dump(moviment_data, f, indent=4)
-        print("Contenuto salvato in 'moviment_list.json'\n")
+        with open('warehouse_list.json', 'w') as f:
+            json.dump(warehouse_data, f, indent=4)
+        print("Contenuto salvato in 'warehouse_list.json'\n")
 
-        return moviment_data.get('results', [])
+        return warehouse_data.get('results', [])
     else:
-        print("Errore durante la richiesta a /moviment/list:", response.status_code, response.text)
+        print("Errore durante la richiesta a /warehouse/list:", response.status_code, response.text)
         return []
 
-# Funzione per fare una richiesta POST a /product/list e ottenere i prodotti
-def get_product_list(token):
+# Funzione per fare una richiesta POST a /eshop/list info su eshops
+def get_eshop_list(token):
     headers = {
         'Authorization': f'Bearer {token}',  
         'Content-Type': 'application/json'
     }
     
-    response = requests.post(product_list_url, headers=headers)
+    response = requests.post(eshop_list_url, headers=headers)
 
     if response.status_code == 200:
         # Ottieni la risposta JSON
-        product_data = response.json()
+        eshop_data = response.json()
         print("Scarico la lista prodotti in formato JSON")
 
         # Salva la risposta JSON in un file
-        with open('product_list.json', 'w') as f:
-            json.dump(product_data, f, indent=4)
-        print("Contenuto salvato in 'product_list.json'\n")
+        with open('eshop_list.json', 'w') as f:
+            json.dump(eshop_data, f, indent=4)
+        print("Contenuto salvato in 'eshop_list.json'\n")
 
-        return product_data.get('results', [])
+        return eshop_data.get('results', [])
     else:
-        print("Errore durante la richiesta a /product/list:", response.status_code, response.text)
+        print("Errore durante la richiesta a /eshop/list:", response.status_code, response.text)
         return []
 
-# Funzione per convertire una data in formato desiderato
+# Funzione per convertire una data dal formato ISO a GG:MM:AAAA HH:MM:SS
 def format_date(date_str):
     if isinstance(date_str, str) and date_str:
         try:
-            # Converte la stringa in un oggetto datetime (gestisce anche il fuso orario)
+            
             date_obj = datetime.fromisoformat(date_str)
-            # Converte l'oggetto datetime nel formato desiderato (gg-mm-aaaa hh:mm:ss)
             return date_obj.strftime('%d-%m-%Y %H:%M:%S')
         except ValueError as e:
             print(f"Errore nella conversione della data: {e}")
@@ -178,13 +177,13 @@ def format_date(date_str):
         return None
 
 # Funzione per generare il CSV e caricarlo su SFTP
-def generate_csv_and_upload_to_sftp(expedition_list, carrier_list, moviment_list, product_list):
+def generate_csv_and_upload_to_sftp(expedition_list, carrier_list, warehouse_list, eshop_list):
     timestamp = datetime.now().strftime('%d-%m-%Y_%H:%M:%S')  
     remote_file_path = f'/home/wsitalagro/webapps/ws-italagro/orders/export_orders_api_{timestamp}.csv'  
     
     print("Genero il CSV aggiornato...")
     csv_buffer = StringIO()
-    fieldnames = ['Order Number', 'Order Date', 'Carrier', 'Status', 'Tracking Number', 'Tracking Url', 'Delivery Date', 'Delivery Firstname', 'Delivery Lastname', 
+    fieldnames = ['Order Number', 'Order Date', 'Status', 'E-Shop', 'Warehouse', 'WMS', 'Carrier', 'Tracking Number', 'Tracking Url', 'Delivery Date', 'Delivery Firstname', 'Delivery Lastname', 
                   'Delivery Street', 'Delivery House nr', 'Delivery Zip', 'Delivery City', 'Delivery Country', 'Delivery Email', 'Delivery Phone', 'Packages Count', 'SKU Total', 'Piece Total', 'Value', 'Currency']
     
     writer = csv.DictWriter(csv_buffer, fieldnames=fieldnames)
@@ -201,19 +200,32 @@ def generate_csv_and_upload_to_sftp(expedition_list, carrier_list, moviment_list
         formatted_order_date = format_date(order_date_str)
         formatted_delivery_date = format_date(delivery_date_str)
 
-        # Trova il nome del corriere associato alla spedizione
         carrier_name = ""
         for carrier in carrier_list:
             if carrier.get('id') == expedition.get('carrier'):  
                 carrier_name = carrier.get('name', '')
-                break  # Esci dal ciclo non appena trovi il corriere   
+                break  
 
-        # Scrivi una riga nel CSV
+        eshop_name = ""
+        for eshop in eshop_list:
+            if eshop.get('id') == expedition.get('eshop'):
+                eshop_name = eshop.get('name', '')
+                break
+
+        warehouse_name = ""
+        for warehouse in warehouse_list:
+            if warehouse.get('id') == expedition.get('warehouse'):
+                warehouse_name = warehouse.get('name', '')
+                break
+
         new_row = {
             'Order Number': expedition.get('orderNumber', ''),
             'Order Date': formatted_order_date,
-            'Carrier': carrier_name,
             'Status': expedition.get('status', ''),
+            'E-Shop': eshop_name,
+            'Warehouse': warehouse_name, 
+            'WMS': warehouse_name, 
+            'Carrier': carrier_name,
             'Tracking Number': expedition.get('trackingNumber', ''),
             'Tracking Url': expedition.get('trackingUrl', ''),
             'Delivery Date': formatted_delivery_date,
@@ -279,11 +291,11 @@ def authenticate():
 
                 expedition_list = get_expedition_list(token)
                 carrier_list = get_carrier_list(token)
-                moviment_list = get_moviment_list(token)
-                product_list = get_product_list(token)
+                warehouse_list = get_warehouse_list(token)
+                eshop_list = get_eshop_list(token)
 
-                if expedition_list and carrier_list and moviment_list and product_list:
-                    generate_csv_and_upload_to_sftp(expedition_list, carrier_list, moviment_list, product_list)
+                if expedition_list and carrier_list and warehouse_list and eshop_list:
+                    generate_csv_and_upload_to_sftp(expedition_list, carrier_list, warehouse_list, eshop_list)
                 break 
 
 # Schedulazione salvataggio CSV ogni 5 minuti
@@ -293,9 +305,6 @@ schedule.every(1).minutes.do(authenticate)
 while True:
     schedule.run_pending()
     time.sleep(10)  
-
-
-
 
 
 
